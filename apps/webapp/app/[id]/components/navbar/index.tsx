8e4eb65dbfed97091b800434fab5a57b566b4f06
Enrico Moschella <41647483+Enrico724@users.noreply.github.com>
2024-07-12 20:33:09 +0200

    added: navbar

---
+++ apps/webapp/app/[id]/components/navbar/index.tsx
@@ -0,0 +1,28 @@
+
+"use client";
+
+import { Avatar, Dropdown, Navbar } from "flowbite-react";
+import { UserDropdown } from "./userDropdown";
+
+export default function AppNavbar() {
+  return (
+    <Navbar fluid rounded>
+      <Navbar.Brand href="https://flowbite-react.com">
+        <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">OnePlace</span>
+      </Navbar.Brand>
+      <div className="flex md:order-2">
+        <UserDropdown/>
+        <Navbar.Toggle />
+      </div>
+      <Navbar.Collapse>
+        <Navbar.Link href="#" active>
+          Home
+        </Navbar.Link>
+        <Navbar.Link href="#">About</Navbar.Link>
+        <Navbar.Link href="#">Services</Navbar.Link>
+        <Navbar.Link href="#">Pricing</Navbar.Link>
+        <Navbar.Link href="#">Contact</Navbar.Link>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+}
---
+++ apps/webapp/app/[id]/page.tsx
@@ -0,0 +1,8 @@
+import AppNavbar from "./components/navbar";
+import { Searchbar } from "./components/navbar/searchbar";
+
+export default function ContentPage() {
+    return (
+        <AppNavbar/>
+    );
+}
