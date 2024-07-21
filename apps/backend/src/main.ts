5a01ef6e3ca683b70567f4d92a375ab567778172
Enrico724 <>
2024-07-22 00:46:28 +0200

    fix: CORS

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -4,19 +4,27 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors()
-
+  
   if (process.env.NODE_ENV === 'development') {
+    app.enableCors({
+      origin: 'http://localhost:3000',
+      credentials: true,
+    });
     const options = new DocumentBuilder()
-      .setTitle('Your API')
-      .setDescription('API description')
-      .setVersion('1.0')
-      .build();
-
+    .setTitle('Your API')
+    .setDescription('API description')
+    .setVersion('1.0')
+    .build();
+    
     const document = SwaggerModule.createDocument(app, options);
     SwaggerModule.setup('api', app, document);
+  } else {
+    app.enableCors({
+      origin: 'http://api.oneplace.lol',
+      credentials: true,
+    });
   }
-
+  
   await app.listen(3001);
 }
 bootstrap();
